4d194f16c47499b86656786be4fd7d97333d87be
Thasanthan-codethamizha <sivakumarthasanthan@gmail.com>
2021-09-28 17:06:23 +0530

    started

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import {Container} from 'react-bootstrap';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <main className="py-3">
+        <Container>
+          <h1>Welcome</h1>
+        </Container>
+      </main>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
